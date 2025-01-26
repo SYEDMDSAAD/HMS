970391559652e2708b03d30eba4fa6624f448ef1
SYEDMDSAAD <134770714+SYEDMDSAAD@users.noreply.github.com>
2025-01-26 13:57:33 +0530

    third

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,10 @@ import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import fileUpload from "express-fileupload";
-/*import { errorMiddleware } from "./middlewares/error.js";
+import { errorMiddleware } from "./middlewares/error.js";
 import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
-import appointmentRouter from "./router/appointmentRouter.js";*/
+//import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
 config({ path: "./config/config.env" });
@@ -30,11 +30,11 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
-/*app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
-app.use("/api/v1/appointment", appointmentRouter);*/
+//app.use("/api/v1/appointment", appointmentRouter);
 
 dbConnection();
 
-//app.use(errorMiddleware);
+app.use(errorMiddleware);
 export default app;
