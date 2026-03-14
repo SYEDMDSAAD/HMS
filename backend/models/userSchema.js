abc98bac594b74bc5e4791d882ce46511d53385d
Syed Mohammad Saad <134770714+SYEDMDSAAD@users.noreply.github.com>
2026-03-14 10:57:29 +0000

    updated user schema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -22,14 +22,14 @@ const userSchema = new mongoose.Schema({
   phone: {
     type: String,
     required: [true, "Phone Is Required!"],
-    minLength: [10, "Phone Number Must Contain Exact 10 Digits!"],
-    maxLength: [10, "Phone Number Must Contain Exact 10 Digits!"],
+    minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
+    maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
   },
   nic: {
     type: String,
     required: [true, "NIC Is Required!"],
-    minLength: [5, "NIC Must Contain Only 5 Digits!"],
-    maxLength: [5, "NIC Must Contain Only 5 Digits!"],
+    minLength: [13, "NIC Must Contain Only 13 Digits!"],
+    maxLength: [13, "NIC Must Contain Only 13 Digits!"],
   },
   dob: {
     type: Date,
