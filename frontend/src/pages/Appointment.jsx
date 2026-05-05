490859ebe7657b155eba298a57a9c3e5510e990e
Syed Mohammad Saad <mdsaadsyed29@gmail.com>
2026-05-05 12:00:00 +0530

    updates

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "../components/Hero";
 import AppointmentForm from "../components/AppointmentForm";
 
 const Appointment = () => {
+  useEffect(() => {
+    document.title = "Book an Appointment · Care Medical Institute";
+  }, []);
+
   return (
     <>
       <Hero
-        title={"Schedule Your Appointment | Care Medical Institute"}
-        imageUrl={"/signin.png"}
+        title="Schedule Your Appointment"
+        imageUrl="/signin.png"
+        showActions={false}
       />
-      <AppointmentForm/>
+      <AppointmentForm />
     </>
   );
 };
